7c2407d2a4c0c620ccd85df472d122373bac2579
MatStep <matstepanek2@gmail.com>
2017-03-04 13:42:40 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,174 @@
+import numpy as np
+import glob
+import os
+import random
+from time import time
+from sklearn import svm
+import argparse
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from skimage import io
+from string import digits
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from scipy import interp
+
+def load_images_from_folder(folder):
+    images = []
+    classes = []
+    classIndexes = []
+    for filename in os.listdir(folder):
+        img = io.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+        cl = os.path.splitext(os.path.basename(filename))[0]
+        cl = cl.translate(None, digits)
+        if cl not in classes:
+            classes.append(cl)
+    return images, classes
+
+def load_classes_from_folder(folder, names):
+    classes = []
+    for filename in os.listdir(folder):
+        cl = os.path.splitext(os.path.basename(filename))[0]
+        cl = cl.translate(None, digits)
+        clIndex = names.index(cl)
+        namesIndex = [0] * len(names)
+        namesIndex[clIndex] = 1
+        classes.append(namesIndex)
+    return classes
+
+def compute_pca(X_train, y_train, comparator = LinearRegression()):
+    n_components = min(X_train.shape[0], X_train.shape[1])
+    pipe = Pipeline([('pca', PCA(n_components, svd_solver='randomized')),
+                     ('linear', comparator)])
+    pipe.fit(X_train, y_train)
+    return pipe
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+def pred_approx_val(arr, treshold):
+    array_np = np.copy(arr)
+    low_val_indices = arr < treshold
+    high_val_indices = arr >= treshold
+    array_np[low_val_indices] = 0
+    array_np[high_val_indices] = 1
+    return array_np
+
+def perf_measure(actual, score, treshold):
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+
+    size = len(score)
+    for i in range(size):
+        predArr = pred_approx_val(score[i], treshold)
+        for j in range(len(score[i])):
+            if(predArr[j] != actual[i][j] and predArr[j] == 1):
+                FP+=1
+            if(predArr[j] == actual[i][j] == 1):
+                TP+=1
+            if(predArr[j] != actual[i][j] and predArr[j] == 0):
+                FN+=1
+            if(predArr[j] == actual[i][j] == 0):
+                TN+=1
+    return TP, FP, TN, FN
+
+def calc_far_frr(FP, FN, totalP, totalN):
+    FAR = FP/float(totalP) # False Accept Rate in percentage
+    FRR = FN/float(totalN) # False Reject Rate in percentage
+    return FAR, FRR
+
+def prepare_graph_far_frr(actual, score, totalP, totalN):
+    step = 1
+    far = dict()
+    frr = dict()
+
+    for i in xrange(0, 100, step):
+        _, FP, _, FN = perf_measure(actual, score, i/float(100))
+        far[i], frr[i] = calc_far_frr(FP, FN, totalP, totalN)
+    return far, frr
+
+# Parameters FAR and FRR in dict() format
+def plot_far_frr(far, frr):
+
+    axisVal = np.arange(0,1.00,0.01);
+
+    names = ['id','data']
+    formats = ['f8','f8']
+    dtype = dict(names = names, formats=formats)
+    farArray = np.array(far.values())
+    frrArray = np.array(frr.values())
+
+    idx = np.argwhere(np.diff(np.sign(frrArray - farArray)) != 0)
+    mn = np.mean([farArray[idx], frrArray[idx]])
+
+    # PLOT FAR FRR
+    plt.figure()
+    lw = 2
+    plt.plot(far.values(), axisVal, label='False Accept Rate', color='blue', lw=lw)
+    plt.plot(axisVal, frr.values(), label='False Reject Rate', color='red', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    # plt.plot(axisVal[idx], mn, 'ro', label= 'EER in %0.02f' % axisVal[idx])
+    plt.xlabel('Treshold')
+    plt.ylabel('Errors')
+    plt.title('FAR and FRR')
+    plt.legend(loc="lower right")
+
+def compute_roc(n_classes, y_score, y_test):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    return tpr, fpr, roc_auc
+
+def plot_roc(tpr, fpr, roc_auc):
+    plt.figure()
+    lw = 2
+    plt.plot(fpr["micro"], tpr["micro"], color='darkorange',
+             lw=lw, label='micro-average ROC curve (area = %0.2f)' % roc_auc["micro"])
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+                    color='blue',linestyle=':', linewidth=4)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
---
+++ main.py
@@ -0,0 +1,108 @@
+import numpy as np
+import glob
+import random
+from time import time
+from sklearn import svm
+import argparse
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_predict
+from matplotlib import pyplot as plt
+
+from functions import load_images_from_folder
+from functions import load_classes_from_folder
+from functions import compute_pca
+from functions import unison_shuffled_copies
+from functions import pred_approx_val
+from functions import perf_measure
+from functions import calc_far_frr
+from functions import prepare_graph_far_frr
+from functions import plot_far_frr
+from functions import compute_roc
+from functions import plot_roc
+
+parser = argparse.ArgumentParser(description='Calculating biometric system FAR, FRR and ROC')
+parser.add_argument('--test_size', type=float, default=0.3,
+                    help='Choose the test size in 0-1 float range (default: 0.3)')
+parser.add_argument('--cross_val', type=int, default=4,
+                    help='Choose the cross validation k-folds, in int (default: 4)')
+
+args = parser.parse_args()
+
+# Global variables
+test_percent = args.test_size
+folder_name = "./faces"
+cross_validation = args.cross_val
+
+# Load data from folder
+imageDir = folder_name
+X, names = load_images_from_folder(imageDir)
+X = np.array(X)
+n_samples, h, w = X.shape
+y = load_classes_from_folder(imageDir, names)
+y = np.array(y)
+n_classes = y.shape[1]
+
+# Shuffle array
+X, y = unison_shuffled_copies(X, y)
+
+# Change to lilnear vectors
+linearData=[]
+for i in range(0, len(X)):
+    linearData.append(X[i].ravel())
+
+linearData = np.array(linearData)
+X = linearData
+
+# Shuffle and split training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_percent,
+                                                    random_state=0)
+n_features = X.shape[1]
+
+print("--------------------")
+print("Total dataset size:")
+print("Number of samples in dataset: %d" % n_samples)
+print("Number of featurs (dimension of images): %d" % n_features)
+print("Number of classes in dataset: %d" % n_classes)
+print("--------------------")
+print("Total train size: %d samples, %d%%" % (len(X_train), (1 - test_percent) * 100))
+print("Total test size: %d samples, %d%%" % (len(X_test), test_percent * 100))
+print("Cross validation %d-fold" % cross_validation)
+print("--------------------")
+
+if cross_validation < 2:
+    print "ERROR: cross-validation argument must be greater than 1"
+    quit()
+
+print "Computing PCA and Linear regression"
+t0 = time()
+model = compute_pca(X_train, y_train, LinearRegression())
+print("done in %0.3fs" % (time() - t0))
+
+print "Computing prediction with cross validation"
+t0 = time()
+y_score = cross_val_predict(model, X_test, y_test, cv=cross_validation) # make 4-fold cross validation
+y_score = y_score.clip(0) # clipping negative numbers to zero
+test_size = len(y_score)
+total_comparisions = test_size * n_classes;
+print("done in %0.3fs" % (time() - t0))
+
+# Compute FAR and FRR for graph
+print "Computing FAR and FRR for graph"
+t0 = time()
+far, frr = prepare_graph_far_frr(y_test, y_score, total_comparisions - test_size, test_size)
+print("done in %0.3fs" % (time() - t0))
+
+# Compute ROC curve and ROC area for each class
+print "Computing ROC curve for graph"
+t0 = time()
+tpr, fpr, roc_auc = compute_roc(n_classes, y_score, y_test)
+print("done in %0.3fs" % (time() - t0))
+
+print "Plotting graphs..."
+# Plot FAR and FRR
+plot_far_frr(far, frr)
+
+# Plot ROC
+plot_roc(tpr, fpr, roc_auc)
+plt.show()
